bfa87691c87afd5761052a4e9cdbb5087d44dcdc
penghui <penghui.zhang@foxmail.com>
2025-03-29 21:58:03 +0800

    feat: create orginaztion button

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,8 +1,9 @@
+import NewButton from './new-button'
 
 export default function Sidebar() {
     return (
         <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gapy-y-4 text-white">
-            Side
+            <NewButton />
         </aside>
     )
 }
