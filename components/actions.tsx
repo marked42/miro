0841d5048b8586ec57d51c82f083cd997e6f2809
penghui <penghui.zhang@foxmail.com>
2025-03-31 23:07:15 +0800

    feat: delete board action in context menu

--- components/actions.tsx
+++ components/actions.tsx
@@ -2,8 +2,12 @@
 
 import { DropdownMenuContentProps, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { ConfirmModal } from "./confirm-modal";
+import { Button } from "./ui/button";
 
 export interface ActionProps {
     children: React.ReactNode;
@@ -26,6 +30,17 @@ export const Actions = ({
             .catch(() => toast.error("Failed to copy link"))
     }
 
+    const { pending, mutate } = useApiMutation(api.board.remove)
+    const onDelete = () => {
+        mutate({
+            id
+        }).then(() => {
+            toast.success(`Board ${id} deleted`)
+        }).catch(() => {
+            toast.error("Failed to delete")
+        })
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -41,6 +56,18 @@ export const Actions = ({
                     <Link2 className="h-4 w-4 mr-2"></Link2>
                     Copy board link
                 </DropdownMenuItem>
+                {/* TODO: why wrapping inside a DropDownMenuItem makes modal  close quickly after opened */}
+                <ConfirmModal
+                    header="Delete board?"
+                    description="This will delete board and all of its content"
+                    disabled={pending}
+                    onConfirm={onDelete}
+                >
+                    <Button variant="ghost" className="p-3 cursor-pointer text-sm w-full justify-start font-normal">
+                        <Trash2 className="h-4 w-4 mr-2"></Trash2>
+                        Delete
+                    </Button>
+                </ConfirmModal>
             </DropdownMenuContent>
         </DropdownMenu>
     )
--- convex/board.ts
+++ convex/board.ts
@@ -38,3 +38,20 @@ export const create = mutation({
     return board
   },
 })
+
+export const remove = mutation({
+  args: {
+    id: v.id('board'),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (!identity) {
+      throw new Error('Unauthorized')
+    }
+
+    // TODO: later check to delete favorite relation as well
+
+    await ctx.db.delete(args.id)
+  },
+})
