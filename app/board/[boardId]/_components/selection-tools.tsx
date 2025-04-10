2d37a2c6e28f544360f483e7eb9c7197e36fc04d
penghui <penghui.zhang@foxmail.com>
2025-04-10 22:49:41 +0800

    refactor: toArray is deprecated

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -31,7 +31,7 @@ export const SelectionTools = memo(({ camera, setLastColor }: SelectionToolsProp
         const liveLayerIds = storage.get('layerIds')
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
             if (selection?.includes(arr[i])) {
@@ -48,7 +48,7 @@ export const SelectionTools = memo(({ camera, setLastColor }: SelectionToolsProp
         const liveLayerIds = storage.get('layerIds')
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for (let i = 0; i < arr.length; i++) {
             if (selection?.includes(arr[i])) {
